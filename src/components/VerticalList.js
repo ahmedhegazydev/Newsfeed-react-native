09ff14a0732dd5269adca87cd167fd0bc66269f3
ahmedhegazy <wowrar1234@gmail.com>
2022-04-01 01:53:51 +0200

    exporting the vertical list

---
+++ src/components/VerticalList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import FlatCard from "./FlatCard";
+import Title from "./Title";
+
+export default function VerticalList({ title, data }) {
+  return (
+    <View>
+      <Title>{title}</Title>
+      <View style={styles.container}>
+        {data.map((item) => (
+          <FlatCard item={item} key={item.id} />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 15,
+  },
+});
