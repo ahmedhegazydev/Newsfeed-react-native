e42bba7a3c5de15ab4e16b15d7ccca608e1f862b
ahmedhegazy <wowrar1234@gmail.com>
2022-03-31 21:50:20 +0200

    removing unused code

--- src/navigation/screens/Home.js
+++ src/navigation/screens/Home.js
@@ -1,5 +1,5 @@
- import * as React from 'react';
- import {
+import * as React from "react";
+import {
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -7,29 +7,29 @@
   Text,
   useColorScheme,
   View,
-  TouchableOpacity, 
+  TouchableOpacity,
+} from "react-native";
 
-} from 'react-native';
-
-import { createStackNavigator } from '@react-navigation/stack';
-import ListAllNewsScreen from './ListAllNews';
-import { LIST_ALL_NEWS_NAME } from '../../constants/contants';
+import { createStackNavigator } from "@react-navigation/stack";
+import ListAllNewsScreen from "./ListAllNews";
+import { LIST_ALL_NEWS_NAME } from "../../constants/contants";
 const Stack = createStackNavigator();
 
-
-export default function HomeScreen({navigation}) {
-    return (
+export default function HomeScreen({ navigation }) {
+  return (
     //   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     //     <Text>Home!</Text>
     //   </View>
 
-<Stack.Navigator
- screenOptions={{
-  // headerShown: false
-}}
->
-<Stack.Screen name={LIST_ALL_NEWS_NAME} component={ListAllNewsScreen}/>
-{/* <Stack.Screen component={ListAllNewsScreen}/> */}
-</Stack.Navigator >
-    );
-  }
+    <Stack.Navigator
+      screenOptions={
+        {
+          // headerShown: false
+        }
+      }
+    >
+      <Stack.Screen name={LIST_ALL_NEWS_NAME} component={ListAllNewsScreen} />
+      {/* <Stack.Screen component={ListAllNewsScreen}/> */}
+    </Stack.Navigator>
+  );
+}
