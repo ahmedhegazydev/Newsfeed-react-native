5a691d3a8af856446d2673d6d5a36eac00d91ce6
ahmedhegazy <wowrar1234@gmail.com>
2022-04-01 00:38:43 +0200

    eporting small card

--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -1,31 +1,41 @@
 import * as React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import Title from "./Title";
 import SubTitle from "./SubTitle";
+import BlockCard from "./BlockCard";
 
-export default function SmallCard({ children, style }) {
-  return <View style={[styles.container, style]}></View>;
+const { width } = Dimensions.get("window");
+
+export default function SmallCard({}) {
+  return <BlockCard style={styles.container} imagStyle={styles.image} />;
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: 300,
-
+    width: width / 2,
+    height: 200,
+    marginRight: 15,
     // fontWeight: "bold",
     // fontSize: size,
-    backgroundColor: "white",
-    borderRadius: 8,
+    // backgroundColor: "white",
+    // borderRadius: 8,
     // justifyConent: "center",
-    overflow: "hidden",
+    // overflow: "hidden",
   },
   image: {
-    width: "100%",
-    height: 200,
+    // width: "100%",
+    height: 100,
   },
   contentContainer: {
-    padding: 5,
+    // padding: 5,
     // height: 200,
   },
 });
