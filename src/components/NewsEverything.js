ea946cb1055aa928ceaafc0b2bc0a6c1057c6fd6
ahmedhegazy <wowrar1234@gmail.com>
2022-04-01 02:51:41 +0200

    callling getting all everthing news use effect/state

---
+++ src/components/NewsEverything.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+export default function Title({ children, numberOfLines = 2, size = 18 }) {
+  const styles = StyleSheet.create({
+    title: {
+      fontWeight: "bold",
+      fontSize: size,
+      // backgroundColor: "white",
+      // borderRadius: 8,
+      // justifyConent: "center",
+    },
+    container: {},
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text numberOfLines={numberOfLines}>{children}</Text>
+    </View>
+  );
+}
--- src/navigation/screens/ListAllNews.js
+++ src/navigation/screens/ListAllNews.js
@@ -5,10 +5,32 @@ import {
   Text,
   View,
   TouchableOpacity,
+  useEffect,
+  useState,
 } from "react-native";
 import SearchBar from "../../components/SearchBar";
-
+import newsApi from "../../api/NewApi";
 export default function ListAllNewsScreen({ navigation }) {
+  const [newsEverything, setEverythingNews] = useState([]);
+  // const [breakingNews, setEverythingNews] = useState([]);
+  // const [featuredNews, setEverythingNews] = useState([]);
+  // const [politicalNews, setEverythingNews] = useState([]);
+  // const [entertainmentNews, setEverythingNews] = useState([]);
+
+  const filterMultipleNews = async () => {
+    const allNewsEverything = await newsApi.getAllNewsEverything();
+    // const allFeaturedNews = allNewsEverything
+    //   .filter((item) => item.featured === "on")
+    //   .reverse()[0];
+  };
+
+  useEffect(() => {
+    filterMultipleNews();
+    // return () => {
+    //   effect;
+    // };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>List All News!</Text>
