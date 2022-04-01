d5b87e9a5fee2bf5aba95cd6087159cc97c0f896
ahmedhegazy <wowrar1234@gmail.com>
2022-04-01 02:28:48 +0200

    exporting getAllNewsEverything async func

--- src/api/NewApi.js
+++ src/api/NewApi.js
@@ -1,5 +1,18 @@
 import axios from "axios";
+import apiClient from "./client";
 
-export default axios.create({
-  baseURL: "https://newsapi.org/v2",
-});
+const getAllNewsEverything = async () => {
+  try {
+    const endpoint =
+      "/everything?domains=wsj.com&apiKey=3b677719d8794bdb91440e41130d9449";
+    const response = await apiClient.get(endpoint);
+    return response.articles;
+  } catch (error) {
+    return [];
+    console.log("Errror while getting all everything news ", error.message);
+  }
+};
+
+export default {
+  getAllNewsEverything,
+};
